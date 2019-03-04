eea2c190b6f9fb083881de1242bfdb8dfe7d51d6
Cannicide <41762964+Cannicide@users.noreply.github.com>
2019-03-04 17:06:20 -0500

    Updated NugScript To NodeJS Module
    Made NugScripts a full-on node module to make things easier to use in my code without the need for importing and exporting classes.

--- nugScript.js
+++ nugScript.js
@@ -2,15 +2,25 @@
 everything easier and I don't need to copy and paste old code
 into server.js over and over agian with the fear of data loss*/
 
-export public class nugScript {
+var message = {};
 
-  senpai() {
+var nugScript = {
+
+  setMessage: function(msg) {
+    //Sets the message variable to the message object from the Discord.js library
+    message = msg;
+  }
+  senpai: function() {
     message.channel.send({files: [{
       attachment: "https://raw.githubusercontent.com/Cannicide/project-sif/master/senpei.PNG",
       name: "senpei.PNG"
     }]})
-  }
-  chess(challenger, challenged) {
+  },
+  chess: function(challenger, challenged) {
     message.channel.send("feature not ready yet, go bother nug, very big sad");
   }
+  
 }
+
+//Exports this file as a node module with a nugScript object:
+module.exports = nugScript;
