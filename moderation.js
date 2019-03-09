837e552341760ea8a643e954ac267e26ca53156e
Cannicide <41762964+Cannicide@users.noreply.github.com>
2019-03-09 11:35:14 -0500

    Updating Rewritten Branch
    Adding in the node-modules folder with discord.js module in it, but this folder does not include all of the node modules used by the project.

--- moderation.js
+++ moderation.js
@@ -1,4 +1,5 @@
 var message, guild, id;
+var ls = require("./sif-casino/ls");
 
 
 function initialize(commands, messageVal, guildVal, idVal) {
@@ -26,10 +27,124 @@ function doPurge(args) {
     }
 }
 
+//----------------------------------Guild Modes-------------------------------------
+//Integer mode possible values:
+//0 = disable (null for Whitelist)
+//1 = enable (null for Whitelist)
+//2 = return current enabled status (returns whitelisted users/bots for Whitelist)
+//3 = (adds specified members for Whitelist)
+function guildLockdown(mode) {
+    //Lockdown: Keep every user on the inside in, and every user on the outside out...
+    //Does not apply to bots
+    switch (mode) {
+        case 0:
+
+        break;
+        case 1:
+
+        break;
+        case 2:
+
+        break;
+        case 3:
+            return null;
+        break;
+    }
+}
+
+function guildEnforce(mode) {
+    //Enforce: Prevent any and all bots from joining
+    //Does not apply to users
+    switch (mode) {
+        case 0:
+
+        break;
+        case 1:
+
+        break;
+        case 2:
+
+        break;
+        case 3:
+            return null;
+        break;
+    }
+}
+
+function guildWhiteout(mode) {
+    //Keep everything out except for those already inside your server, and those on the Whitelist
+    switch (mode) {
+        case 0:
+            
+        break;
+        case 1:
+            
+        break;
+        case 2:
+
+        break;
+        case 3:
+            return null;
+        break;
+    }
+}
+
+function guildChateau(mode) {
+    //Prevent people on the whitelist from making invites
+    //(Useful for preventing accidentally whitelisted newbie catastrophes in Guild Whiteout Mode)
+    switch (mode) {
+        case 0:
+            
+        break;
+        case 1:
+            
+        break;
+        case 2:
+
+        break;
+        case 3:
+
+        break;
+    }
+}
+
+function guildWhitelist(mode, args) {
+    //Adds specified people (in args) to the whitelist
+    //Used in Guild Whiteout and Chateau modes
+    var whitelistUsers = false;
+    if (ls.exist(guild.id + "whitelist") && ls.getObj(guild.id + "whitelist").length >= 1) {
+        whitelistUsers = ls.getObj(guild.id + "whitelist");
+    }
+    switch (mode) {
+        case 0:
+            return null;
+        break;
+        case 1:
+            return null;
+        break;
+        case 2:
+            //List of users/bots whitelisted
+        break;
+        case 3:
+            //Add/remove to/from whitelist
+            
+        break;
+    }
+}
+
 const moderation = {
     init: initialize,
     messages: {
         purge: doPurge
+    },
+    guild: {
+        lockdown: guildLockdown,
+        enforce: guildEnforce,
+        whiteout: guildWhiteout,
+        chateau: guildChateau,
+        members: {
+            whitelist: guildWhitelist
+        }
     }
 }
 
